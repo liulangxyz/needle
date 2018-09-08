392faf6e855462aed12d1605342e95e15208c67f
Lang Liu <hello.liulang@gmail.com>
2018-09-08 12:00:34 +0900

    python: remove unnecessary command checking

--- package/python/pythonrc.sh
+++ package/python/pythonrc.sh
@@ -4,17 +4,11 @@
 alias vipythonrc="vi $DOT_HOME/package/python/pythonrc.sh && source $SHELLRC"
 
 # pyenv and pyenv-virtualenv
-if command -v pyenv 1>/dev/null 2>&1 ; then
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-fi
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 # virtualenvwrapper
-if pip3 show virtualenvwrapper 1>/dev/null 2>&1 ; then
-  if command -v python3 1>/dev/null 2>&1 ; then
-    export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-  fi
-  export PIP_REQUIRE_VIRTUALENV=false
-  export WORKON_HOME=$HOME/.python-env
-  source /usr/local/bin/virtualenvwrapper.sh
-fi
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export PIP_REQUIRE_VIRTUALENV=false
+export WORKON_HOME=$HOME/.python-env
+source /usr/local/bin/virtualenvwrapper.sh
