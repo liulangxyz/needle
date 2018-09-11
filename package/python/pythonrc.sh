e9820be40e6a3836b7c7f5c8f80ee51a39867405
Lang Liu <hello.liulang@gmail.com>
2018-09-11 14:29:24 +0900

    python: lazyload virtualenvwrapper and pyenv

--- package/python/pythonrc.sh
+++ package/python/pythonrc.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# lazy load pyenv
+loadpyenv() {
+  # pyenv and pyenv-virtualenv
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+}
+
+# lazy load virtualenvwrapper
+loadvirtualenvwrapper() {
+  # virtualenvwrapper
+  export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+  export PIP_REQUIRE_VIRTUALENV=false
+  export WORKON_HOME=$HOME/.python-env
+  source /usr/local/bin/virtualenvwrapper.sh
+}
+
 # alias
 alias vipythonrc="vi $DOT_HOME/package/python/pythonrc.sh && source $SHELLRC"
-
-# pyenv and pyenv-virtualenv
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
-# virtualenvwrapper
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export PIP_REQUIRE_VIRTUALENV=false
-export WORKON_HOME=$HOME/.python-env
-source /usr/local/bin/virtualenvwrapper.sh
