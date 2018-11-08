9b5dd261b087666f8965634d3e419356056d9a5b
Lang Liu <hello.liulang@gmail.com>
2018-11-08 13:53:13 +0900

    shell: fed fd output to fzf

--- plugin/shell.plugin.sh
+++ plugin/shell.plugin.sh
@@ -23,3 +23,7 @@ alias mk="cd ~/Documents"
 # macos
 alias showfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+
+# fzf
+export FZF_DEFAULT_COMMAND='fd --type f'
