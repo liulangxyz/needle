9c910d666dc07c9700121a6ad6e910e76beca6c2
Lang Liu <hello.liulang@gmail.com>
2018-12-07 10:47:39 +0900

    plugin:shell: move variable configuration to shell

--- plugin/shell.plugin.sh
+++ plugin/shell.plugin.sh
@@ -21,3 +21,7 @@ alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO; killall F
 
 # fzf
 export FZF_DEFAULT_COMMAND='fd --type f'
+
+# ruby
+export PATH="/usr/local/opt/ruby/bin:$PATH"
+export PATH="/usr/local/lib/ruby/gems/2.5.0/bin:$PATH"
