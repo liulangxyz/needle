8756740d7d8a2a467582dcf27a9e9a482171276d
Lang Liu <hello.liulang@gmail.com>
2019-09-05 13:41:00 +0900

    use brewinstall instead of brew install

--- needle.sh
+++ needle.sh
@@ -7,17 +7,17 @@ set -e
 ########################################
 # output string in green
 info() {
-  echo $'\e[1;32m'"[NEEDLE]"$'\e[0m' "${@:1}"
+  echo $'\e[1;32m'"[NEEDLE]"$'\e[0m' $*
 }
 
-# $1 brew
+# $1: brew
 brewinstall() {
   info "brew install $1"
   brew install "$1"
 }
 
-# $1 source
-# $2 target
+# $1: source
+# $2: target
 symlink() {
   info "symlink $2"
   ln -fsv "$1" "$2"
@@ -60,7 +60,7 @@ brewinstall httpie
 # git
 ########################################
 # install latest git
-brew install git
+brewinstall git
 
 # gitignore
 symlink "$PWD"/conf/git/gitignore ~/.gitignore
@@ -81,22 +81,23 @@ fi
 ########################################
 # node
 ########################################
-brew install node
+brewinstall node
 
 # node version manager
 if [ ! -s ~/.nvm/nvm.sh ]; then
   info "install node version manager"
   git clone https://github.com/creationix/nvm.git ~/.nvm
+  local CURR_DIR=$PWD
   cd ~/.nvm
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-  popd
+  cd "$CURR_DIR"
 fi
 
 
 ########################################
 # python
 ########################################
-brew install python3
+brewinstall python3
 
 # pip
 [[ ! -d ~/.pip ]] && mkdir ~/.pip
@@ -106,8 +107,8 @@ symlink "$PWD"/conf/pip/pip.conf ~/.pip/pip.conf
 ########################################
 # tmux
 ########################################
-brew install tmux
-brew install reattach-to-user-namespace
+brewinstall tmux
+brewinstall reattach-to-user-namespace
 
 # tmux.conf
 symlink "$PWD"/conf/tmux/tmux.conf ~/.tmux.conf
@@ -116,17 +117,18 @@ symlink "$PWD"/conf/tmux/tmux.conf ~/.tmux.conf
 ########################################
 # vim
 ########################################
-brew install vim
+brewinstall vim
 
 # vimrc
 symlink "$PWD"/conf/vim/vimrc ~/.vimrc
 
 # install plugin manager
 if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
-  echo "install plug.vim"
+  info "install plug.vim, a vim plugin manager"
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+info "config vim plugins"
 read -r -p "do you want to install vim plugins (y/n): " ans
 if [[ $ans = 'y' ]]; then
   vim +PlugInstall +qall
@@ -138,15 +140,15 @@ if [[ $ans = 'y' ]]; then
 fi
 
 # override default vi
-if [[ ! -h /usr/local/bin/vi && -f /usr/local/bin/vim ]]; then
-  ln -fsv /usr/local/bin/vim /usr/local/bin/vi
+if [[ -f /usr/local/bin/vim ]]; then
+  symlink /usr/local/bin/vim /usr/local/bin/vi
 fi
 
 
 ########################################
 # zsh
 ########################################
-brew install zsh
+brewinstall zsh
 
 # oh my zsh
 if [[ ! -d ~/.oh-my-zsh ]]; then
